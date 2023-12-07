206fd807645b6bd579770074fe946d9efd22b0a7
Ankit Varshney <lyfflame45@gmail.com>
2023-12-08 02:47:30 +0530

    appointment screen

--- lib/features/meetings/screens/meeting_screen.dart
+++ lib/features/meetings/screens/meeting_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:justice_link/common/app_bar.dart';
+import 'package:justice_link/features/meetings/screens/appointment_screen.dart';
 import 'package:justice_link/features/meetings/widgets/lawyer_card.dart';
 
 class Meeting extends StatelessWidget {
@@ -9,10 +10,15 @@ class Meeting extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appbarfun("Meetings"),
-      body: const SingleChildScrollView(
+      body: SingleChildScrollView(
         child: Column(
           children: [
-            LawyerCard(),
+            GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const AppointmentScreen()));
+                },
+                child: LawyerCard()),
             LawyerCard(),
             LawyerCard(),
             LawyerCard(),
