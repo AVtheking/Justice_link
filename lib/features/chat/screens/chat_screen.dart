d7f6f36986e1c7f58d540da9ea8cbdc248c4a475
ilakshaygupt <gupta22154006@akgec.ac.in>
2023-12-14 18:36:29 +0530

    added socket sen message

--- lib/features/chat/screens/chat_screen.dart
+++ lib/features/chat/screens/chat_screen.dart
@@ -1,71 +1,77 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:justice_link/features/auth/services/auth_service.dart';
 
 import 'package:justice_link/features/chat/widgets/chat_widgets.dart';
+import 'package:justice_link/global.dart';
 import 'package:justice_link/models/meeting.dart';
+import 'package:justice_link/models/user.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
 
-class ChatScreen extends StatefulWidget {
+class ChatScreen extends ConsumerStatefulWidget {
   const ChatScreen({Key? key, this.meeting}) : super(key: key);
   final Meeting? meeting;
   @override
-  State<ChatScreen> createState() => _ChatScreenState();
+  ConsumerState<ChatScreen> createState() => _ChatScreenState();
 }
 
-class _ChatScreenState extends State<ChatScreen> {
+class _ChatScreenState extends ConsumerState<ChatScreen> {
+  late IO.Socket socket;
   final ScrollController _scrollController = ScrollController();
-
   final List<Message> _messageList = [
     const Message(
         content: "Hi, Bill! This is the simplest example ever.",
         ownerType: OwnerType.sender,
         ownerName: "Higor Lapa"),
-    const Message(
-        content:
-            "Let's make it better , Higor. Custom font size and text color",
-        textColor: Colors.black38,
-        ownerType: OwnerType.receiver,
-        ownerName: "Bill Gates"),
-    const Message(
-        content: "Bill, we have to talk about business",
-        ownerType: OwnerType.sender,
-        ownerName: "Higor"),
-    const Message(
-        content: "Wow, I like it. Tell me what I can do for you, pal.",
-        ownerType: OwnerType.receiver,
-        ownerName: "Bill Gates"),
-    const Message(
-        content: "I'm just a copy",
-        ownerType: OwnerType.sender,
-        ownerName: "Higor"),
-    const Message(
-        content: "Nice",
-        ownerType: OwnerType.receiver,
-        ownerName: "Bill Gates"),
-    const Message(
-        content: "I'm just a copy",
-        ownerType: OwnerType.sender,
-        ownerName: "Higor"),
-    const Message(
-        content: "Nice",
-        ownerType: OwnerType.receiver,
-        ownerName: "Bill Gates"),
-    const Message(
-        content: "I'm just a copy",
-        ownerType: OwnerType.receiver,
-        ownerName: "Bill Gates"),
-    const Message(
-        content: "Nice",
-        ownerType: OwnerType.receiver,
-        ownerName: "Bill Gates"),
-    const Message(
-        content: "I'm just a copy",
-        ownerType: OwnerType.sender,
-        ownerName: "Higor"),
-    const Message(
-        content: "I'm just a copy",
-        ownerType: OwnerType.sender,
-        ownerName: "Higor"),
   ];
+  @override
+  void initState() {
+    super.initState();
+    // connectToServer();
+  }
+
+  void connectToServer() {
+    socket = IO.io('$uri', <String, dynamic>{
+      'transports': ['websocket'],
+      'autoconnect': false,
+    });
+    socket.connect();
+    socket.on('connect', (_) {
+      print('Connected to server');
+    });
+    socket.on(
+      'message',
+      (data) {
+        print("hih----------------------------------------------i");
+        print(_messageList);
+        final messageData = data['message'];
+        print(data);
+        final user = ref.read(userProvider);
+        // final user = User(name: 'name', email: 'email', password: 'password');
+        if (messageData is Map<String, dynamic>) {
+          final message;
+          if (messageData['userId'] == user?.id) {
+            message = Message(
+                content: messageData['message'],
+                ownerType: OwnerType.sender,
+                ownerName: user?.name);
+          } else {
+            message = Message(
+              content: messageData['message'],
+              ownerType: OwnerType.sender,
+              ownerName: user?.name,
+            );
+          }
+          _messageList.add(message);
+        }
+        print(_messageList);
+        setState(
+          () {},
+        );
+      },
+    );
+  }
 
   final TextEditingController _textEditingController = TextEditingController();
 
@@ -84,7 +90,9 @@ class _ChatScreenState extends State<ChatScreen> {
           Padding(
             padding: const EdgeInsets.only(right: 8.0),
             child: IconButton(
-              onPressed: () {},
+              onPressed: () {
+                connectToServer();
+              },
               icon: const Icon(
                 Icons.call,
                 color: Colors.white,
@@ -103,85 +111,103 @@ class _ChatScreenState extends State<ChatScreen> {
               children: _messageList,
             ),
           ),
-          widget.meeting!=null ? 
-          widget.meeting?.meetingStatus == 'pending' ? Container():
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(20)),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        controller: _textEditingController,
-                        decoration: const InputDecoration(
-                          hintText: 'Type a message...',
+          widget.meeting != null
+              ? widget.meeting?.meetingStatus == 'pending'
+                  ? Container()
+                  : Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: TextField(
+                                  controller: _textEditingController,
+                                  decoration: const InputDecoration(
+                                    hintText: 'Type a message...',
+                                  ),
+                                ),
+                              ),
+                              IconButton(
+                                icon: const Icon(Icons.photo),
+                                onPressed: () {},
+                              ),
+                              IconButton(
+                                icon: const Icon(Icons.send),
+                                onPressed: () {
+                                  _sendMessage(_textEditingController.text);
+                                },
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.photo),
-                      onPressed: () {},
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.send),
-                      onPressed: () {
-                        _sendMessage(_textEditingController.text);
-                      },
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ):   Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(20)),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        controller: _textEditingController,
-                        decoration: const InputDecoration(
-                          hintText: 'Type a message...',
-                        ),
+                    )
+              : Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20)),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: TextField(
+                              controller: _textEditingController,
+                              decoration: const InputDecoration(
+                                hintText: 'Type a message...',
+                              ),
+                            ),
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.photo),
+                            onPressed: () {},
+                          ),
+                          IconButton(
+                            icon: const Icon(Icons.send),
+                            onPressed: () {
+                              _sendMessage(_textEditingController.text);
+                            },
+                          ),
+                        ],
                       ),
                     ),
-                    IconButton(
-                      icon: const Icon(Icons.photo),
-                      onPressed: () {},
-                    ),
-                    IconButton(
-                      icon: const Icon(Icons.send),
-                      onPressed: () {
-                        _sendMessage(_textEditingController.text);
-                      },
-                    ),
-                  ],
+                  ),
                 ),
-              ),
-            ),
-          ),
         ],
       ),
     );
   }
 
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    socket.disconnect();
+    super.dispose();
+  }
+
   void _sendMessage(String text) {
+    final user = ref.read(userProvider);
+
     if (text.isNotEmpty) {
-      _messageList.add(
-        Message(
-          content: text,
-          ownerType: OwnerType.sender,
-          ownerName: "Your Name",
-        ),
-      );
+      // _messageList.add(
+      //   Message(
+      //     content: text,
+      //     ownerType: OwnerType.sender,
+      //     ownerName: "Your Name",
+      //   ),
+      // );
+      Map<String, dynamic> data = {
+        'userId': user?.id,
+        'message': text,
+      };
+      socket.emit('message', data);
       _textEditingController.clear();
 
       _scrollController.animateTo(
@@ -189,8 +215,6 @@ class _ChatScreenState extends State<ChatScreen> {
         duration: const Duration(milliseconds: 200),
         curve: Curves.easeIn,
       );
-
-      setState(() {});
     }
   }
 }
